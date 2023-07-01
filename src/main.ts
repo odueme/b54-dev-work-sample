0f2383b0fec3bcfa57a96322d496d02efcc24cc1
Uzoezi <uodueme@gmail.com>
2023-07-01 09:51:33 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://fanciful-treacle-ff4cc5.netlify.app/',
+    origin: 'https://fanciful-treacle-ff4cc5.netlify.app/#/',
     credentials: true,
   });
 
