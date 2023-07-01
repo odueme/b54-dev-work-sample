b74396cec8a7f7171390c4b91ea94f19322227f0
Uzoezi <uodueme@gmail.com>
2023-07-01 03:44:01 +0100

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
-import { CartModule } from './cart/cart.module';
-import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Users } from './auth/user.entity';
-import { CartEntity } from './cart/cart.entity';
-import { ProductEntity } from './product/product.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import{ProductModule} from './product/product.module'
+import {AuthModule} from './auth/auth.module'
+import {CartModule} from './cart/cart.module'
+import {AppController} from './app.controller'
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
-
-
-
+import {AppService} from './app.service'
 
 
 @Module({
   imports: [
-    ProductModule, 
-    AuthModule, 
-    CartModule, 
-   
-    ConfigModule.forRoot({ isGlobal: true }),
-  TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    useFactory: (configService: ConfigService) =>({
-    type: 'postgres',
-    host: configService.get('DB_HOST'),
-    port: +configService.get('DB_PORT'), 
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
-    entities: [CartEntity, Users, ProductEntity],
-    synchronize: true,
-  }),
-  inject: [ConfigService]
-  }),
-
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductModule,
+    AuthModule,
+    CartModule,
   ],
   controllers: [AppController],
   providers: [AppService],
-
 })
 export class AppModule {}
