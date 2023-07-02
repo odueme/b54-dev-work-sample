abc7e6414792199b04b0c5176dff2ac3b9d340c6
Uzoezi <uodueme@gmail.com>
2023-07-02 09:52:17 +0100

    changes

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -4,10 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './cart.entity';
 import { ProductService } from 'src/product/product.service';
 import { Users } from 'src/auth/user.entity';
-import { ConfigService } from '@nestjs/config';
-
-
-
+const accountSid = 'AC75105157ee5d0bc3ce92eaa1d573d067'
+const authToken = '15d0ebf5ad34a01c10efdcae68ad2425'
+const client = require('twilio')(accountSid, authToken)
 @Injectable()
 export class CartService {
   
@@ -17,7 +16,6 @@ export class CartService {
     @InjectRepository(Users)
     private userRepository: Repository<Users>,
     private productsService: ProductService,
-    private readonly configService: ConfigService,
   ) {}
 
   async addToCart(
@@ -25,24 +23,19 @@ export class CartService {
     quantity: number,
     user: string,
   ): Promise<any> {
-    const accountSid = this.configService.get<string>('TWILIO_ACCOUNT_SID');
-    const authToken = this.configService.get<string>('TWILIO_AUTH_TOKEN');
-    const client = require('twilio')(accountSid, authToken)
     const cartItems = await this.cartRepository.find({
       relations: ['item', 'user'],
     });
+    const arr = []
+    arr.push(quantity)
+    const sum = arr.reduce((acc, cur)=> {
+      return acc + cur
+    }, 0)
     const product = await this.productsService.getOne(productId);
     const authUser = await this.userRepository.findOneBy({ username: user });
     
     //Confirm the product exists.
     if (product) {
-      //confirm if user has item in cart
-      const cart = cartItems.filter(
-        (item) => item.item.id === productId && item.user.username === user,
-      );
-
-     
-      if (cart.length < 1) {
         client.messages.create({
           from: '+14177964331',
           to: `+234${authUser.phoneNumber}`,
@@ -55,26 +48,25 @@ export class CartService {
         return await this.cartRepository.save({
           user: user,
           item: item,
-          total: product.price * quantity,
-          quantity,
+          total: product.price * sum,
+          sum,
         });
-      } else {
-       
-        //Update the item quantity
-        const quantity = (cart[0].quantity += 1);
-        const total = cart[0].total * quantity;
+        
+    
+      //else {
+         
+      //   const total = cart[0].quantity * product.price
+      //   client.messages.create({
+      //     from: '+14177964331',
+      //     to: '+23409023752601',
+      //     body: `Hello ${authUser.username} this is your order Name: ${product.name} price: ${total} quantity: ${quantity}` 
+      //   })
 
-        client.messages.create({
-          from: '+14177964331',
-          to: '+23409023752601',
-          body: `Hello ${authUser.username} this is your order Name: ${product.name} price: ${total} quantity: ${quantity}` 
-        })
-
-        return await this.cartRepository.update(cart[0].id, {
-          quantity,
-          total,
-        });
-      }
+      //   return await this.cartRepository.update(cart[0].id, {
+      //     quantity: cart[0].quantity += quantity,
+      //     total: product.price * quantity,
+      //   });
+      // }
     }
     return null;
   }
