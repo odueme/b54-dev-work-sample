84e901c49e0d685c722069ac6e522ce2580191c7
Uzoezi <uodueme@gmail.com>
2023-07-02 10:10:27 +0100

    changes

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -26,50 +26,42 @@ export class CartService {
     const cartItems = await this.cartRepository.find({
       relations: ['item', 'user'],
     });
-    const arr = []
-    arr.push(quantity)
-    const sum = arr.reduce((acc, cur)=> {
-      return acc + cur
-    }, 0)
+  
     const product = await this.productsService.getOne(productId);
     const authUser = await this.userRepository.findOneBy({ username: user });
-    
-    //Confirm the product exists.
-    if (product) {
-        client.messages.create({
-          from: '+14177964331',
-          to: `+234${authUser.phoneNumber}`,
-          body: `Hello ${authUser.username} this is your order Name: ${product.name} price: ${product.price} quantity: ${quantity}` 
-        })
-      
-        const user = authUser;
-        const item = product;
-
-        return await this.cartRepository.save({
-          user: user,
-          item: item,
-          total: product.price * sum,
-          quantity: sum,
-        });
-        
-    
-      //else {
-         
-      //   const total = cart[0].quantity * product.price
-      //   client.messages.create({
-      //     from: '+14177964331',
-      //     to: '+23409023752601',
-      //     body: `Hello ${authUser.username} this is your order Name: ${product.name} price: ${total} quantity: ${quantity}` 
-      //   })
-
-      //   return await this.cartRepository.update(cart[0].id, {
-      //     quantity: cart[0].quantity += quantity,
-      //     total: product.price * quantity,
-      //   });
-      // }
+  
+    // Confirm the product and user exist.
+    if (product && authUser) {
+      client.messages.create({
+        from: '+14177964331',
+        to: `+234${authUser.phoneNumber}`,
+        body: `Hello ${authUser.username}, this is your order. Name: ${product.name}, price: ${product.price}, quantity: ${quantity}`,
+      });
+  
+      const existingCartItem = cartItems.find(
+        (item) =>
+          item.item.id === productId && item.user.username === user
+      );
+  
+      if (existingCartItem) {
+        existingCartItem.quantity += quantity;
+        existingCartItem.total = product.price * existingCartItem.quantity;
+        return await this.cartRepository.save(existingCartItem);
+      }
+  
+      const newItem = this.cartRepository.create({
+        user: authUser,
+        item: product,
+        total: product.price * quantity,
+        quantity: quantity,
+      });
+  
+      return await this.cartRepository.save(newItem);
     }
+  
     return null;
   }
+  
 
   async getItemsInCard(user: string): Promise<CartEntity[]> {
     const userCart = await this.cartRepository.find({
