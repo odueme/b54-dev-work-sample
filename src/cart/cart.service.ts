e0cc64ec72216b6132471fc83c7524ee19532038
Uzoezi <uodueme@gmail.com>
2023-07-07 06:37:29 +0100

    changes

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -5,6 +5,8 @@ import { CartEntity } from './cart.entity';
 import { ProductService } from 'src/product/product.service';
 import { Users } from 'src/auth/user.entity';
 import { ConfigService } from '@nestjs/config';
+import { DeepPartial } from 'typeorm';
+
 
 
 
@@ -21,39 +23,41 @@ export class CartService {
     @InjectRepository(Users)
     private userRepository: Repository<Users>,
     private productService: ProductService,
-    private configService: ConfigService,
+   
 
   ) {}
 
   async addToCart(productId: number, quantity: number, user: string): Promise<any> {
-    const cartItems = await this.cartRepository.find({ relations: ["item",'user'] });
+    const cartItems = await this.cartRepository.find({ relations: ['item', 'user'] });
     const product = await this.productService.getOne(productId);
-    const authUser = await this.userRepository.findOneBy({ username: user })
-   
-    //Confirm the product exists.
+    const authUser = await this.userRepository.findOneBy({ username: user });
+  
+    // Confirm the product exists.
     if (product) {
-      
-        const cart = cartItems.filter(
-            (item) => item.item.id === productId && item.user.username === user,
-        );
-        if (cart.length < 1) {
-
-            const newItem = this.cartRepository.create({ total: product.price * quantity, quantity });
-            newItem.user = authUser;
-            newItem.item = product;
-            this.cartRepository.save(newItem)
-
-
-            return await this.cartRepository.save(newItem)
-        } else {
-          const quantity = (cart[0].quantity += 1);
-          const total = cart[0].total * quantity;
-
-          return await this.cartRepository.update(cart[0].id, { quantity, total });
+      const existingCartItem = cartItems.find(
+        (item) => item.item.id === productId && item.user.username === user
+      );
+  
+      if (existingCartItem) {
+        existingCartItem.quantity += quantity;
+        existingCartItem.total = product.price * existingCartItem.quantity;
+        return await this.cartRepository.save(existingCartItem);
       }
+  
+      const newItem = this.cartRepository.create({
+        user: authUser,
+        item: product,
+        total: product.price * quantity,
+        quantity: quantity,
+        price: product.price, // Add this line to set the price property
+      }  as DeepPartial<CartEntity>)
+  
+      return await this.cartRepository.save(newItem);
+    }
+  
+    return null;
   }
-  return null;
-}
+  
 
   async getItemsInCard(user: string): Promise<CartEntity[]> {
     const userCart = await this.cartRepository.find({
